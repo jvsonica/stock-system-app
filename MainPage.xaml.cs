b1ed899106e7f2249546d1901c8f055b557a9883
Leonel Araújo <leonel.araujo27@gmail.com>
2015-12-04 00:58:22 +0000

    basic app structure. http request to our sv to subscribe stock

---
+++ MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using StockExchangeSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace StockExchangeSystem
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private ObservableCollection<Stock> stocks;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            stocks = new ObservableCollection<Stock>();
+            stocksList.ItemsSource = stocks;
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            // TODO: Verify if it exists
+            
+            stocks.Add(new Stock(tbox1.Text.ToString()));
+            tbox1.Text = "";
+
+        }
+
+        private void btn_more_info_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using Windows.Data.Json;
+
+namespace StockExchangeSystem.Models
+{
+    public class Stock : INotifyPropertyChanged
+    {
+        private String symbol;
+        private String name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Stock(string symbol)
+        {
+            this.Symbol = symbol;
+            getStockName(symbol);
+        }
+
+        private async void getStockName(string symbol)
+        {
+
+            string url = "https://cmovstocksystem.herokuapp.com/stock/add?user=1&name="+symbol+"&lower=0&upper=300";
+            Uri uri = new Uri(url);
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(uri);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string answer = await response.Content.ReadAsStringAsync();
+                JsonObject json = JsonObject.Parse(answer);
+                this.Name = json.GetNamedString("name");
+            }
+            else if(response.StatusCode == HttpStatusCode.BadRequest)
+            {
+
+            }
+        }
+
+        public string Symbol
+        {
+            get
+            {
+                return this.symbol;
+            }
+            set
+            {
+                this.symbol = value;
+                NotifyPropertyChanged("Symbol");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
